f3b3c787934988fda24fc3dba559f92562662386
sakshi Aherkar <sakshiaherkar17@gmail.com>
2023-03-26 13:07:00 +0530

    commited

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
+import Header from '@/components/header'
 import '@/styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+    
+    <MoralisProvider initializeOnMount={false}>
+      <Header/>
+      <Component {...pageProps} />
+    </MoralisProvider>
+    </>
+    
+  )
 }
